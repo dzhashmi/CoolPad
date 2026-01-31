9b6a00e4ee112c19711de55b1a217da8beb04d7c
dzhashmi <daniyalzahidhashmi7737@gmail.com>
2026-01-31 01:49:39 -0800

    Update rules.mk

--- firmware/rules.mk
+++ firmware/rules.mk
@@ -3,17 +3,17 @@ MCU = RP2040
 BOOTLOADER = rp2040
 
 # Key Matrix
-MATRIX_DRIVER = custom # We will define pins in info.json
+MATRIX_DRIVER = custom 
 
 # Features
-MOUSEKEY_ENABLE = yes   # Mouse keys (optional)
-EXTRAKEY_ENABLE = yes   # Audio control and System control
-CONSOLE_ENABLE  = yes   # Console for debug
-COMMAND_ENABLE  = no    # Commands for debug and configuration
-NKRO_ENABLE     = yes   # USB N-Key Rollover
+MOUSEKEY_ENABLE = yes   
+EXTRAKEY_ENABLE = yes   
+CONSOLE_ENABLE  = yes   
+COMMAND_ENABLE  = no    
+NKRO_ENABLE     = yes   
 
-# Nexus Pad Specifics
-ENCODER_ENABLE  = yes   # Enable the Rotary Encoder
-OLED_ENABLE     = yes   # Enable the OLED Screen
-RGBLIGHT_ENABLE = yes   # Enable the underglow LEDs
+ENCODER_ENABLE  = yes   #Enable the Rotary Encoder
+OLED_ENABLE     = yes   #Enable the OLED Screen
+RGBLIGHT_ENABLE = yes   #Enable the underglow LEDs
+
 WS2812_DRIVER_REQUIRED = yes # Required for RP2040 LEDs
